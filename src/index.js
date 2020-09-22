1cdc917395939377ab270401d863c1d9e2b276b5
tungocvan <tungocvan@gmail.com>
2020-09-22 09:04:36 +0700

    nodejs_blog-master

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
+const route = require('./routes');
+
+const app = express();
+const port = 3000;
+
+// Use static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Routes init
+route(app);
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`),
+);
